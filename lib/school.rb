b01de03051bd9a874c83064205b75581860985c0
Rwanda S <rwanda.ace@gmail.com>
2020-03-23 18:20:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,12 @@ class School
   end
   
   def add_student(name, grade)
+    
+  if @roster[grade].length > 0
+    @roster[grade] << [name]
+  else 
     @roster[grade] = [name]
+  end
   end
 
 end
