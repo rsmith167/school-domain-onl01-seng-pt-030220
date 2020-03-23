b4142ada977ba6fc2bffd936bb1a0f04c71ca924
Rwanda S <rwanda.ace@gmail.com>
2020-03-23 18:44:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+  
+  
+  def initialize(name)
+  @name = name
+  @roster = {}
+  end
+  
+  def add_student(name, grade)
+    
+  if @roster[grade] != nil
+    @roster[grade] << name
+  else 
+    @roster[grade] = [name]
+  end
+  end
+  
+  def roster
+    @roster
+  end
+    def grade(grade)
+      @roster[grade]
+    end
+    def sort
+      return_hash = {}
+    @roster.each do |key, value|
+      return_hash[key] =  @roster[key].sort
+    end
+    return_hash
+  end
+end
